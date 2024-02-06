1b8c6e853785a38b55ddbef06ae85333b3170fdf
Erin Clark <erincodepirate@gmail.com>
2024-02-05 20:53:05 -0600

    Add stdscr functions

--- tutorial1.py
+++ tutorial1.py
@@ -3,5 +3,8 @@ from curses import wrapper
 
 def main(stdscr):
     stdscr.clear()
+    stdscr.addstr(10, 10, "hello world", curses.A_UNDERLINE)
+    stdscr.refresh()
+    stdscr.getch()
 
 wrapper(main)
